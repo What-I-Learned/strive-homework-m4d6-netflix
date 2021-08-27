72f681068f56899a0630e5a64ec9105bcfb4646e
Wakodono <53127265+Wakodono@users.noreply.github.com>
2021-08-27 13:16:39 +0100

    moviesection updates

--- src/components/MovieSection.jsx
+++ src/components/MovieSection.jsx
@@ -1,13 +1,21 @@
 import { Container, Row, Col } from 'react-bootstrap'
 
-const movieSection = () => {
+const MovieSection = () => {
   return (
-    <Container>
+    <Container className="container-fluid pl-lg-5 mt-3">
       <Row>
-        <Col className="px-1"><img src="https://place-hold.it/341x192" alt="" /></Col>
+          <h3>Action</h3>
+        <div className="d-flex flex-nowrap">
+            <Col className="px-1"><img src="https://place-hold.it/341x192" alt="" /></Col>
+            <Col className="px-1"><img src="https://place-hold.it/341x192" alt="" /></Col>
+            <Col className="px-1"><img src="https://place-hold.it/341x192" alt="" /></Col>
+            <Col className="px-1"><img src="https://place-hold.it/341x192" alt="" /></Col>
+            <Col className="px-1"><img src="https://place-hold.it/341x192" alt="" /></Col>
+            <Col className="px-1"><img src="https://place-hold.it/341x192" alt="" /></Col>
+        </div>
       </Row>
     </Container>
   );
 };
 
-export default movieSection
+export default MovieSection
