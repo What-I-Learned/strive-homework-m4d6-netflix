fa9a360357098701680f3fa74656ed4e7f13c56b
js25s <petrauskas.justas@gmail.com>
2021-08-27 16:43:54 +0200

    Update SingleComment.jsx

--- src/components/SingleComment.jsx
+++ src/components/SingleComment.jsx
@@ -1,8 +1,8 @@
 import { Carousel } from "react-bootstrap";
 
-const SingleComment = ({ comment }) => (
-  <Carousel.Item className="comment-carousel-item">
-    <Carousel.Caption>{comment.comment}</Carousel.Caption>
+const SingleComment = ({ comment, key }) => (
+  <Carousel.Item className="comment-carousel-item" key={key}>
+    <h2>{comment.comment}</h2>
   </Carousel.Item>
 );
 export default SingleComment;
