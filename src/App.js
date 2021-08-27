e5591d3318f44be342b31676d296623879e88ea2
js25s <petrauskas.justas@gmail.com>
2021-08-27 13:04:48 +0200

    started on single movie

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 import Footer from "./components/Footer";
+import SingleMovie from "./components/SingleMovie";
 
 function App() {
   return (
     <div className="App">
       <header></header>
-      <main></main>
+      <main>
+        <SingleMovie />
+      </main>
       <Footer />
     </div>
   );
