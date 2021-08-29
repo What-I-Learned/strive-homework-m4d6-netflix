e6529572897829dae8fa2b290ffadb03b1b70384
js25s <petrauskas.justas@gmail.com>
2021-08-29 03:33:48 +0200

    going well

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import "@splidejs/splide/dist/css/themes/splide-default.min.css";
 import "./App.css";
 
-import Navigation from "./components/Navigation";
+import MovieSegment from "./components/MovieSegment";
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <Navigation />
-      </header>
-      <main></main>
+      <header></header>
+      <main>
+        <MovieSegment name={"horror"} />
+        <MovieSegment name={"adventure"} />
+        <MovieSegment name={"comedy"} />
+        <MovieSegment name={"harry potter"} />
+      </main>
       <footer></footer>
     </div>
   );
